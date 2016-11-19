f1931697d5622fa1c5622952eaee3d0ca1e0c4e2
Tanrada <tanradap@gmail.com>
2016-11-19 22:32:56 +0000

    mainC should be cool

--- src/taskC/helper.py
+++ src/taskC/helper.py
@@ -20,36 +20,6 @@ us = io.us
 gyro = io.gyro
 col = io.col
 
-def find_nearest(lowest, highest):
-    """
-    servo the UltrasonicSensor and find the nearest object to it
-    :param - lowest :
-    :param
-    """
-    servo.run_to_abs_pos(position_sp=lowest)  # min
-    time.sleep(1)
-    min_value = sonar.value()
-    min_pos = motor3.position
-    while motor3.position in range(lowest - 20, highest - 20):
-        motor3.run_to_abs_pos(position_sp=highest)  # max
-        sonar.value()
-        if sonar.value() < min_value:
-            min_value = sonar.value()
-            min_pos = motor3.position
-            print(min_pos, min_value, '*')
-        else:
-            print(motor3.position, sonar.value())
-    print('im done')
-    time.sleep(1)
-    # print min_pos
-    # print min_value
-    while motor3.position not in range(min_pos - 10, min_pos + 10):
-        motor3.run_to_abs_pos(position_sp=min_pos)
-    # return min_pos
-    return min_value, min_pos
-
-
-
 def turn_CW(v, angle, motor):
     """
     turn the robot/servo motor on the spot by the desired_angle by referencing using the gyro value/servo position
@@ -67,37 +37,42 @@ def turn_CW(v, angle, motor):
 
     if motor == 'ROBOT':
         angle = gyro.value() + abs(angle)
-        turn_control = Controller(.5, 0, 0.01,
+        turn_control = Controller(.2, 0, 0.5,
                                   angle,
                                   history=10)
-        R.polarity = 'inversed'
+
+        # L will move forward, R will move backwards
+        if L.duty_cycle_sp < 0 : L.duty_cycle_sp = -1 * L.duty_cycle_sp
+        if R.duty_cycle_sp > 0 : R.duty_cycle_sp = -1 * R.duty_cycle_sp
         while True:
             signal, err = turn_control.control_signal(gyro.value())
-            R.run_timed(time_sp=100, speed_sp=v + abs(signal))
-            L.run_timed(time_sp=100, speed_sp=v + abs(signal))
+            R.run_timed(time_sp=100, speed_sp=v+signal)
+            L.run_timed(time_sp=100, speed_sp=v-signal)
             logging.info('GYRO = {},\tcontrol = {},\t err={}, \tL = {}, \tR = {}'.format(
                 gyro.value(), signal, err, L.speed_sp, R.speed_sp))
-            if abs(signal) <= 1 or io.btn.backspace:  # tolerance
-                R.stop()
+
+            if abs(err) <= 4 or io.btn.backspace:  # tolerance
                 L.stop()
-                L.speed_sp = v
+                R.stop()
                 R.speed_sp = v
-                R.polarity = 'normal'
+                L.speed_sp = v
+                R.duty_cycle_sp = -1 *  R.duty_cycle_sp
                 return
 
     elif motor == 'SERVO':
         angle = servo.position + abs(angle)
-        turn_control = Controller(.5 , 0, 0,
+        turn_control = Controller(.8 , 0.01, 0.01,
                                   angle,
                                   history=10)
-        servo.polarity = 'normal'
+        # check that the servo is moving in positive direction
+        if servo.duty_cycle_sp < 0 : servo.duty_cycle_sp = -1 * servo.duty_cycle_sp
         while True:
             # servo.run_timed(time_sp=100, duty_cycle_sp=v + signal)  # changed from speed_sp
             signal, err = turn_control.control_signal(servo.position)
             servo.run_timed(time_sp=100, speed_sp=v + abs(signal))
             logging.info('POS = {},\tcontrol = {},\t err={}, \tspd = {}'.format(
                 servo.position, signal, err, servo.speed_sp))
-            if abs(signal) <= 1 or io.btn.backspace:  # tolerance
+            if abs(err) <= 4 or io.btn.backspace:  # tolerance
                 servo.stop()
                 servo.speed_sp = v
                 return
@@ -121,41 +96,42 @@ def turn_CCW(v, angle, motor):
 
     if motor == 'ROBOT':
         angle = gyro.value() - abs(angle)
-        turn_control = Controller(.5, 0, 0.01,
+        turn_control = Controller(.2, 0, 0.5,
                                   angle,
                                   history=10)
-        L.polarity = 'inversed'
+        # R will move forward, L will move backwards
+        if R.duty_cycle_sp<0: R.duty_cycle_sp = -1 * R.duty_cycle_sp
+        if L.duty_cycle_sp>0: L.duty_cycle_sp = -1 * L.duty_cycle_sp
         while True:
             signal, err = turn_control.control_signal(gyro.value())
-            R.run_timed(time_sp=100, speed_sp=v + abs(signal))
-            L.run_timed(time_sp=100, speed_sp=v + abs(signal))
+            R.run_timed(time_sp=100, speed_sp=v-signal)
+            L.run_timed(time_sp=100, speed_sp=v+signal)
             logging.info('GYRO = {},\tcontrol = {},\t err={}, \tL = {}, \tR = {}'.format(
                 gyro.value(), signal, err, L.speed_sp, R.speed_sp))
-            if abs(signal) <= 1 or io.btn.backspace:  # tolerance
+            if abs(err) <= 4 or io.btn.backspace:  # tolerance
                 R.stop()
                 L.stop()
-                # reset the settings
-                L.speed_sp = v
+                L.speed_sp = v # reset the speed
                 R.speed_sp = v
-                L.polarity = 'normal'
+                L.duty_cycle_sp = -1 * L.duty_cycle_sp
                 return
 
     elif motor == 'SERVO':
-        angle = servo.position + abs(angle) # desired angle
-        turn_control = Controller(.5, 0, 0,
+        angle = servo.position - abs(angle) # desired angle
+        turn_control = Controller(.8, 0.01, 0.01,
                                   angle,
                                   history=10)
-        servo.polarity = 'inversed'
+        if servo.duty_cycle_sp > 0: servo.duty_cycle_sp = -1 * servo.duty_cycle_sp
         while True:
             signal, err = turn_control.control_signal(servo.position)
             servo.run_timed(time_sp=100, speed_sp=v + abs(signal))
             logging.info('POS = {},\tcontrol = {},\t err={}, \tspd = {}'.format(
                 servo.position, signal, err, servo.speed_sp))
-            if abs(signal) <= 1 or io.btn.backspace:  # tolerance
+            if abs(err) <= 4 or io.btn.backspace:  # tolerance
                 servo.stop()
                 # reset the settings
-                speed_sp=v
-                servo.polarity = 'normal'
+                servo.speed_sp=v
+                servo.duty_cycle_sp = -1 * servo.duty_cycle_sp
                 return
 
 
@@ -170,14 +146,14 @@ def follow_until_dist(v, desired_col, desired_distance):
     global col, L, R, us
 
     ev3.Sound.speak(
-        'Following black line until distance {}'.format(desired_distance)).wait()
+        'Following black line until sonar sensor {}'.format(desired_distance)).wait()
     # defines the line control so that the motor follows the line
     # TODO: need to tune this!
-    line_control = Controller(.5, 0, 0,
+    line_control = Controller(.5, 0, 0.01,
                               desired_col,
                               history=10)  # a P controller
     distance_subject = Subject('distance_subject')
-    distance_to_record = desired_distance*2
+    distance_to_record = desired_distance*1.5 #TODO:try 1.5???
     #record_ = Listener('record_', distance_subject,
 #                       desired_distance*2, 'LT') # to start recording how many taco counts for the range
     halt_ = Listener('halt_', distance_subject,
@@ -191,15 +167,15 @@ def follow_until_dist(v, desired_col, desired_distance):
         distance_subject.set_val(us.value())  # update the subject
         if not initial_position and us.value() in range(distance_to_record-5,distance_to_record+5):
             # ev3.Sound.speak('I will note this position {}'.format(L.position)).wait()
-            initial_position = L.position
+            initial_position = (L.position+R.position)/2
             logging.info('I will note this position {}'.format(initial_position))
 
         if halt_.get_state():  # need to halt since distance have reached
             ev3.Sound.speak('Object detected at range {}'.format(
                 us.value())).wait()  # inform user
             logging.info('STOP!')
-            final_position = L.position
-            diff_position = final_position - initial_position
+            final_position = (L.position+R.position)/2
+            diff_position = (final_position - initial_position)*2
             ev3.Sound.speak('Tacho count travelled is {}'.format(diff_position)).wait()
             logging.info(diff_position)
             L.duty_cycle_sp = v
@@ -237,7 +213,7 @@ def forward_until_line(v, line_col, desired_heading):
                               history=10)  # a P controller
     col_subject = Subject('col_subject')
     halt_ = Listener('halt_',col_subject ,
-                     desired_col, 'LT')  # halt when LT (because it is black)
+                     line_col, 'LT')  # halt when LT (because it is black)
 
     while True:
         col_subject.set_val(col.value())  # update color
@@ -245,12 +221,12 @@ def forward_until_line(v, line_col, desired_heading):
         if halt_.get_state():  # need to halt since distance have reached
             ev3.Sound.speak('Line detcted. hurray!').wait()
             logging.info('STOP!')
-            L.speed_sp = v
-            R.speed_sp = v
-            return diff_position
+            L.duty_cycle_sp = v
+            R.duty_cycle_sp = v
+            return
 
         else:  # when out of range value is not reached yet- keep tracing the object and adjusting to maintain desired_range
-            signal, err = desired_angle_control.control_signal(gyro.value())
+            signal, err = line_control.control_signal(gyro.value())
             if err < 0:
                 L.run_timed(time_sp=100, duty_cycle_sp=v - signal)
                 R.run_timed(time_sp=100, duty_cycle_sp=v + signal)
@@ -337,14 +313,15 @@ def blind_forward(v,tacho_counts, expected_heading):
     current_heading = gyro.value(0)
     ev3.Sound.speak('My current heading is {}.'.format(current_heading)).wait()
     discount = expected_heading - current_heading
-    if discount > 0: # turn c
-        ev3.Sound.speak('I need to turn clockwise {} degrees'.format(discount)).wait()
-        turn_CW(v=20, angle=discount, motor='ROBOT')
-        time.sleep(2) # wait
-    elif discount < 0:
-        ev3.Sound.speak('I need to turn counter clockwise {} degrees'.format(discount)).wait()
-        turn_CCW(v=20, angle=abs(discount), motor='ROBOT')
-        time.sleep(2) # wait
+    if abs(discount) >= 10:
+        if discount > 0: # turn c
+            ev3.Sound.speak('I need to turn clockwise {} degrees'.format(discount)).wait()
+            turn_CW(v=20, angle=discount, motor='ROBOT')
+            time.sleep(2) # wait
+        elif discount < 0:
+            ev3.Sound.speak('I need to turn counter clockwise {} degrees'.format(discount)).wait()
+            turn_CCW(v=20, angle=abs(discount), motor='ROBOT')
+            time.sleep(2) # wait
 
     # execute moving forward:
     ev3.Sound.speak('I will travel extra tacho counts of {}'.format(tacho_counts)).wait()
--- src/taskC/mainC.py
+++ src/taskC/mainC.py
@@ -37,16 +37,14 @@ L.connected
 R.connected
 L.reset()  # reset the settings
 R.reset()
-L.duty_cycle_sp = 40
-R.duty_cycle_sp = 40
+L.duty_cycle_sp = 30
+R.duty_cycle_sp = 30
 servo.connected
 servo.reset()
-servo.duty_cycle_sp = 20
+servo.duty_cycle_sp = 35
 # SENSORS
 col.connected
 col.mode = 'COL-REFLECT'
-# gyro.connected
-# gyro.mode = 'GYRO-CAL'
 us.connected
 us.mode = 'US-DIST-CM'
 
@@ -54,6 +52,8 @@ us.mode = 'US-DIST-CM'
 # CALIBRATION
 # --------------------------------------------------------------------
 ev3.Sound.speak('hello').wait()
+
+logging.info('-------------------CALIBRATION-------------------')
 ev3.Sound.speak('Calibrating, WHITE').wait()
 while True:
     if io.btn.backspace:
@@ -69,8 +69,6 @@ while True:
         ev3.Sound.speak('Done').wait()
         print('MIDPOINT = {}'.format(MIDPOINT))
         break
-
-logging.info('-------------------CALIBRATION-------------------')
 logging.info('MIDPOINT = {}'.format(MIDPOINT))
 
 # # # --------------------------------------------------------------------
@@ -79,98 +77,99 @@ logging.info('MIDPOINT = {}'.format(MIDPOINT))
 logging.info('-------------------RUNNING-------------------')
 logging.info('follow the line until object at desired_distance found')
 # -----------------------------------------------------
+
+
 diff_position = helper.follow_until_dist(v=20,
                    desired_col=MIDPOINT,
-                   desired_distance=150)
+                   desired_distance=100)
 # HEADING FORWARD
-gyro.connected
-gyro.mode = 'GYRO-CAL'  # calibrate the gyro first
-time.sleep(2)
+ev3.Sound.speak('Calibrating')
+gyro.mode = 'GYRO-CAL'
+time.sleep(7)
 gyro.mode = 'GYRO-ANG'
 robot_forward_heading = gyro.value()
 robot_right = robot_forward_heading + 90
 robot_left  = robot_forward_heading - 90
 
-servo_forward = servo.position
-
 logging.info('reference heading = {}'.format(robot_forward_heading))
-
 logging.info('turning 90degrees cw')
-helper.turn_CW(v=30, angle=robot_right-gyro.value(), motor='ROBOT')
-time.sleep(2)
+helper.turn_CW(v=40, angle=robot_right-gyro.value(), motor='ROBOT')
+# time.sleep(2)
 
 logging.info('turn servo 90 degrees')
 # TODO: use function to find nearest
-helper.turn_CCW(v=20, angle=90, motor='SERVO')
+helper.turn_CCW(v=30, angle=90, motor='SERVO')
 time.sleep(2)
 
 # ----->>>---------------------------------------------
 #  HEADING SIDEWAY
 logging.info('MOVING SIDEWAY')
-threshold = 150
+threshold = 50
 logging.info('Moving robot until threshold = {} is exceeded'.format(
                 threshold)) # once us detects surrounding more than 1.5cm away, halt
 helper.move_in_range(30,
                     robot_right,
                     threshold)
-time.sleep(2)
+# time.sleep(2)
 
 logging.info('Moving the robot tacho count = {} to maintain distance from object boundary'.format(
                 diff_position))
 helper.blind_forward(v=30,
                     tacho_counts = diff_position,
                     expected_heading=robot_right)
-time.sleep(2)
+# time.sleep(2)
 
 logging.info('Turning ROBOT CCW')
-helper.turn_CCW(v=30, angle=(gyro.value()-robot_forward_heading), motor='ROBOT')
-time.sleep(2)
+helper.turn_CCW(v=40, angle=(gyro.value()-robot_forward_heading), motor='ROBOT')
+# time.sleep(2)
 
 # --------------------------------------------------------
 # HEADING FORWARD
 logging.info('Moving the robot tacho count = {} for object to be in range'.format(
-                diff_position+100))
+                diff_position+10))
 helper.blind_forward(v=30,
-                    tacho_counts=diff_position+100,
+                    tacho_counts=diff_position+10,
                     expected_heading = robot_forward_heading)
-time.sleep(2)
+# time.sleep(2)
 
 logging.info('Moving forward until edge is found')
 helper.move_in_range(30,
                     robot_forward_heading,
                     threshold)
-time.sleep(2)
+# time.sleep(2)
 
 logging.info('Moving the robot tacho count = {} for object to be in range'.format(
-                    diff_position+300))
+                    diff_position+100))
 helper.blind_forward(v=30,
-                    tacho_counts=diff_position+300, # compensate the length of the robot
+                    tacho_counts=diff_position+100, # compensate the length of the robot
                     expected_heading= robot_forward_heading)
-time.sleep(2)
+# time.sleep(2)
 
 
 # -----<<<<---------------------------------------------
 # HEADING SIDEWAY
 logging.info('turning the ROBOT CCW by {}'.format(robot_forward_heading-90))
-helper.turn_CCW(v=30, angle=(robot_left-gyro.value()), motor='ROBOT')
-time.sleep(2)
+helper.turn_CCW(v=40, angle=(robot_left-gyro.value()), motor='ROBOT')
+# time.sleep(2)
 
 logging.info('find the line!')
 helper.forward_until_line(v=20,
                     line_col = MIDPOINT,
                     desired_heading = robot_left)
-time.sleep(2)
+# time.sleep(2)
 
 
 logging.info('Turning the robot CW by {}'.format(robot_forward_heading-gyro.value()))
-helper.turn_CW(v=30, angle=robot_forward_heading-gyro.value())
-time.sleep(2)
+helper.turn_CW(v=30, angle=robot_forward_heading-gyro.value(), motor='ROBOT')
+# time.sleep(2)
 
-loggingg.info('Following the line').wait()
+logging.info('Following the line').wait()
 control = Controller(kp, ki, kd, MIDPOINT, history)
 v = 30 # constant speed
 while col.value() < WHITE:  # run for 10 seconds
     signal, err = control.control_signal(col.value())
     L.run_timed(time_sp=50, duty_cycle_sp=v-signal) # going CCW
     R.run_timed(time_sp=50, duty_cycle_sp=v+signal)
+    print('HELLO')
+
 exit()

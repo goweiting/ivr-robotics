2c9f49e5d8a72324eb5270ec4410674345fb7ec1
s1450710@sms.ed.ac.uk <s1450710@sms.ed.ac.uk>
2016-11-20 23:33:14 +0000

    stop testing because no more battery~

--- src/taskC/mainC.py
+++ src/taskC/mainC.py
@@ -8,8 +8,6 @@
 # - To complete a lap of a closed loop circuit, which includes circumventing the obstacles and finding the line again.
 # - Have the robot speak at each stage where it think it is!
 
-
-# python import
 import time
 import logging
 
@@ -19,6 +17,8 @@ import util.io as io
 import helper
 from util.control import Controller
 from util.observer import Listener, Subject
+from util.robot import Robot
+from util.turning import turn_on_spot
 
 logging.basicConfig(format='%(levelname)s: %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -32,16 +32,6 @@ us = io.us
 gyro = io.gyro
 col = io.col
 
-# MOTOR:
-L.connected
-R.connected
-L.reset()  # reset the settings
-R.reset()
-L.duty_cycle_sp = 30
-R.duty_cycle_sp = 30
-servo.connected
-servo.reset()
-servo.duty_cycle_sp = 35
 # SENSORS
 col.connected
 col.mode = 'COL-REFLECT'
@@ -71,28 +61,8 @@ while True:
         break
 logging.info('MIDPOINT = {}'.format(MIDPOINT))
 
-# # # --------------------------------------------------------------------
-# # # FOLLOW THE LINE UNTIL IT FOUND AN OBJECT
-# # # --------------------------------------------------------------------
-logging.info('-------------------RUNNING-------------------')
-logging.info('follow the line until object at desired_distance found')
-# -----------------------------------------------------
-
-
-def main():
-    """
-    Start the procedure of following the line until object at desired_distance
-    A series of functions is defined in helper.py
-    """
-
-    global MIDPOINT, gyro, col, L, R
-
-    # FINDING OBJECT
-    diff_position = helper.follow_until_dist(v=30,
-                       desired_col=MIDPOINT,
-                       desired_distance=100)
-
-    # HEADING FORWARD
+def calibrate_gyro():
+    global gyro
     ev3.Sound.speak('Calibrating Gyroscope')
     gyro.mode = 'GYRO-CAL'
     time.sleep(7)
@@ -100,98 +70,134 @@ def main():
     robot_forward_heading = gyro.value()
     robot_right = robot_forward_heading + 90
     robot_left  = robot_forward_heading - 90
+    ev3.Sound.speak('Done')
+    return (robot_forward_heading, robot_left, robot_right)
 
-    if gyro.value() != robot_forward_heading: # TODO
-        ev3.Sound.speak('Calibration Error')
-        return False
+(robot_forward_heading, robot_left, robot_right) = calibrate_gyro()
 
+if gyro.value() != robot_forward_heading: # TODO
+    ev3.Sound.speak('Calibration Error')
+    calibrate_gyro()
+
+# MOTOR:
+L.reset()  # reset the settings
+R.reset()
+L.duty_cycle_sp = 30
+R.duty_cycle_sp = 30
+servo.connected
+servo.reset()
+servo.duty_cycle_sp = 40
+
+robot = Robot()
+tacho_counts_to_travel = robot.get_tacho_counts(20)*15 # calculates tacho counts for 15 cm with duty_cycle of 40
+
+def main():
+    """
+    Start the procedure of following the line until object at desired_distance
+    A series of functions is defined in helper.py
+    """
+
+    global MIDPOINT, gyro, col, L, R, robot_forward_heading, robot_left, robot_right
+
+    # # # --------------------------------------------------------------------
+    # # # FOLLOW THE LINE UNTIL IT FOUND AN OBJECT
+    # # # --------------------------------------------------------------------
+    logging.info('-------------------RUNNING-------------------')
+    logging.info('follow the line until object at desired_distance found')
+    # -----------------------------------------------------
+
+    # FINDING OBJECT
+    helper.follow_until_dist(v=20,
+                       desired_col=MIDPOINT,
+                       desired_distance=100) #  = 10 cm
+
+    # HEADING FORWARD
     logging.info('reference heading = {}'.format(robot_forward_heading))
     logging.info('turning 90degrees cw')
-    helper.turn_CW(v=50,
+    turn_on_spot(v=30,
                     angle=robot_right-gyro.value(),
                     motor='ROBOT')
-    # time.sleep(2)
+    time.sleep(2)
 
     logging.info('turn servo 90 degrees')
-    helper.turn_CCW(v=40,
-                    angle=90, # should use find object instead because not reproducible
-
+    turn_on_spot(v=45,
+                    angle=-90, # should use find object instead because not reproducible
                     motor='SERVO')
     time.sleep(2)
 
     # ----->>>---------------------------------------------
     #  HEADING SIDEWAY
     logging.info('MOVING SIDEWAY')
-    thresh = 50
+    thresh = 40
     logging.info('Moving robot until threshold = {} is exceeded'.format(
                     thresh)) # once us detects surrounding more than 1.5cm away, halt
     helper.move_in_range(v=30,
                         desired_angle=robot_right,
                         threshold=thresh)
-    # time.sleep(2)
+    time.sleep(2)
 
     logging.info('Moving the robot tacho count = {} to maintain distance from object boundary'.format(
-                    diff_position))
-    helper.blind_forward(v=30,
-                        tacho_counts = diff_position,
+                    tacho_counts_to_travel))
+    helper.blind_forward(v=20,
+                        tacho_counts = tacho_counts_to_travel,
                         expected_heading=robot_right)
-    # time.sleep(2)
+    time.sleep(2)
 
     logging.info('Turning ROBOT CCW')
-    helper.turn_CCW(v=50,
-                    angle=(gyro.value()-robot_forward_heading),
+    turn_on_spot(v=30,
+                    angle=-(gyro.value()-robot_forward_heading),
                     motor='ROBOT')
-    # time.sleep(2)
+    time.sleep(2)
 
     # --------------------------------------------------------
     # HEADING FORWARD
     logging.info('Moving the robot tacho count = {} for object to be in range'.format(
-                    diff_position+10))
-    helper.blind_forward(v=30,
-                        tacho_counts=diff_position+10,
+                    tacho_counts_to_travel+10))
+    helper.blind_forward(v=20,
+                        tacho_counts=tacho_counts_to_travel+10,
                         expected_heading = robot_forward_heading)
-    # time.sleep(2)
+    time.sleep(2)
 
     logging.info('Moving forward until edge is found')
     helper.move_in_range(v=30,
                         desired_angle=robot_forward_heading,
                         threshold = thresh)
-    # time.sleep(2)
+    time.sleep(2)
 
     logging.info('Moving the robot tacho count = {} for object to be in range'.format(
-                        diff_position))
-    helper.blind_forward(v=30,
-                        tacho_counts=diff_position,
+                        tacho_counts_to_travel))
+    helper.blind_forward(v=20,
+                        tacho_counts= tacho_counts_to_travel,
                         expected_heading= robot_forward_heading)
-    # time.sleep(2)
+    time.sleep(2)
 
 
     # -----<<<<---------------------------------------------
     # HEADING SIDEWAY
     logging.info('turning the ROBOT CCW by {}'.format(robot_forward_heading-90))
-    helper.turn_CCW(v=50,
-                    angle=(robot_left-gyro.value()),
+    turn_on_spot(v=30,
+                    angle=-(robot_left-gyro.value()),
                     motor='ROBOT')
-    # time.sleep(2)
+    time.sleep(2)
 
     logging.info('find the line!')
     helper.forward_until_line(v=30,
                             line_col = MIDPOINT,
                             desired_heading = robot_left)
-    # time.sleep(2)
+    time.sleep(2)
 
     # ------------------------------------------------------
     logging.info('Moving forward before turning')
-    helper.blind_forward(v=30,
-                        tacho_counts = 100, # TODO: move 100 tacho count?
+    helper.blind_forward(v=20,
+                        tacho_counts = tacho_counts_to_travel, # TODO: move 100 tacho count?
                         expected_heading = robot_left)
-    # time.sleep(2)
+    time.sleep(2)
 
     logging.info('Turning the robot CW by {}'.format(robot_forward_heading-gyro.value()))
-    helper.turn_CW(v=50,
-                    angle=robot_forward_heading-gyro.value(),
+    turn_on_spot(v=30,
+                    angle=gyro.value()-robot_forward_heading,
                     motor='ROBOT')
-    # time.sleep(2)
+    time.sleep(2)
 
     if col.value() > MIDPOINT: # cant find the left edge yet, so turn
         control = Controller(kp=.6, ki=.01, kd=.2,
@@ -204,9 +210,17 @@ def main():
             R.run_timed(time_sp=50, duty_cycle_sp=v-signal)
 
         return # reached midpoint
+
     else:
         return
 
-
-while not io.btn.enter: # use Enter as a circuit breaker
-    main()
+# -------------------------
+# MAIN
+# -------------------------
+ev3.Sound.speak('Press Enter When ready!').wait()
+while True:
+    if io.btn.enter:
+        ev3.Sound.speak('Begin in 3 2 1').wait()
+        main()
+        ev3.Sound.speak('continue').wait()
+        continue
--- src/util/passiveObserver.py
+++
@@ -1,8 +0,0 @@
-"""
-Create a class for passive listening to the position of the robot for Dead reckoning purpose
-"""
-
-import io
-import observer
-
-class PassiveObserver()
